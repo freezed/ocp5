ae687d85ac9cfab00162f0839a5c0447b0116668
Fred Z <freezed@users.noreply.github.com>
2018-07-31 15:59:14 +0200

    Updates SQL for DB creation script #2
    - Adds UNIQUE KEY
    - Deletes association table
    - Adds product's url

--- create-db-loff.sql
+++ create-db-loff.sql
@@ -6,27 +6,24 @@ DROP DATABASE IF EXISTS loff;
 CREATE DATABASE loff CHARACTER SET 'utf8';
 USE loff;
 
+CREATE TABLE category(
+    `id`INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(200) UNIQUE
+
+)ENGINE=InnoDB;
+
 CREATE TABLE product(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `code` BIGINT UNSIGNED NOT NULL UNIQUE,
-    `name` VARCHAR(200),
-    `nutrition_grades` VARCHAR(1)
-);
-
-CREATE TABLE category(
-    `id`INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(200)
-);
-
-CREATE TABLE asso_prod_cat(
-    `category_id` INT UNSIGNED NOT NULL,
-    `product_id` INT UNSIGNED NOT NULL,
-    CONSTRAINT `fk_asso_prod_cat_category`
-        FOREIGN KEY(category_id)
-        REFERENCES category(id)
+    `url` VARCHAR(200),
+    `name` VARCHAR(200) UNIQUE,
+    `nutrition_grades` VARCHAR(1),
+    `category_id`INT UNSIGNED,
+    `substitute_id` INT UNSIGNED,
+    CONSTRAINT `fk_product_category`
+        FOREIGN KEY (category_id) REFERENCES category(id)
         ON DELETE CASCADE,
-    CONSTRAINT `fk_asso_prod_cat_product`
-        FOREIGN KEY(product_id)
-        REFERENCES product(id)
-        ON DELETE CASCADE
-);
+    CONSTRAINT `fk_product_substitute`
+        FOREIGN KEY (substitute_id) REFERENCES product(id)
+        ON DELETE SET NULL
+)ENGINE=InnoDB;
