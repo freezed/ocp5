87199cd4c07c72fb1c3d273b7a9370923cce9ed2
Fred Z <freezed@users.noreply.github.com>
2018-08-04 17:48:58 +0200

    Creates CLI skeleton with comments #5

---
+++ cli.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: freezed <freezed@users.noreply.github.com> 2018-08-04
+Version: 0.1
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+    Command line interface to interact with local DB
+
+Choose a product in the list of aivaiable categories, the system gives you an
+alternative with a better 'nutriscore'.
+You can save this product to get it later.
+
+ """
+
+# Gets category list
+
+# Prompts it with an index
+
+# Asks the user to enter the index of the selected category
+
+    # If index is not valid, re-ask
+
+# Lists all products
+
+# Asks the user to enter the index of the selected product
+
+    # If index is not valid, re-ask
+
+# Shows the 1st product in the same category with the lowest nutriscore
+
+# Saves if user choose it
